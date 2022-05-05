476061777019516361c589486b4e8e883b0f80d1
Timothy Hnat <twhnat@memphis.edu>
2022-05-05 16:41:26 -0500

    Added copyright block

--- python/rl_example.py
+++ python/rl_example.py
@@ -1,6 +1,32 @@
+#  Copyright (c) 2022. University of Memphis, mDOT Center
+# 
+#  Redistribution and use in source and binary forms, with or without modification, are permitted
+#  provided that the following conditions are met:
+# 
+#  1. Redistributions of source code must retain the above copyright notice, this list of conditions
+#  and the following disclaimer.
+# 
+#  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+#  and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 
+#  3. Neither the name of the copyright holder nor the names of its contributors may be used to
+#  endorse or promote products derived from this software without specific prior written permission.
+# 
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+#  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+#  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+#  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+#  OF SUCH DAMAGE.
+# 
+
+
 # %%
 from mdot_reinforcement_learning import reinforcement_learning as mrl
-
 
 server = 'https://localhost:8080/api/v1/rl'
 service_id = '6d93aff2-c619-4695-a5ab-b00ad60759f3'
@@ -28,7 +54,6 @@ possible_codes = [
     'SUCCESS_DATA_VALIDATED',
 ]
 
-
 decision_data = {
     'timestamp': '2020-01-01T00:00:00Z-06:00',
     'data': [
@@ -52,7 +77,6 @@ decision_data = {
     ]
 }
 
-
 # Option 1
 
 try:
@@ -62,12 +86,11 @@ except ValidationError as e:
     pass
 
 try:
-    batch_upload_result = session.batch_upload(validated_data)  # Server validate before saving data, Returns errors if any and the client raises and exception
+    batch_upload_result = session.batch_upload(
+        validated_data)  # Server validate before saving data, Returns errors if any and the client raises and exception
 except SessionError as se:
     pass
 
-
 decision_result = session.decision(decision_data)
-
 
 # %%
