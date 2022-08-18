1a6417d132ff0dbb6880672e4524765a81786e51
Timothy Hnat <twhnat@memphis.edu>
2022-08-17 20:34:32 -0500

    Fixed basic test harness

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,9 +1,9 @@
 import pytest
 
-from pJITAI import interface
+import pJITAI
 
 # Ref: https://github.com/realpython/reader/blob/master/tests/test_feed.py
 
 def test_version():
-    import pJITAI
+    
     print(pJITAI.__version__)
